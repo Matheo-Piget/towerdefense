81b8775cbacb7a0a043ef2132f84634cfc3c5fa2
Mathéo Piget <matheo.piget5@gmail.com>
2023-11-28 15:57:02 +0100

    definition des constructeur de player, enemy et tower

--- Player.java
+++ Player.java
@@ -1,9 +1,11 @@
 public class Player {
+    
     private int money;
-    private int lives;
 
-    public Player(/* initial parameters */) {
-        // Initialization
+    public Player(int money) {
+
+        this.money = money;
+        
     }
 
     // Getters, setters, and methods for managing money, lives, etc.
--- Tower.java
+++ Tower.java
@@ -3,9 +3,11 @@ public class Tower extends Element {
     private int cost;
     private int attackSpeed;
 
-    public Tower(){
+    public Tower(int health, int damage, int cost, int attackSpeed){
 
-
+        super(health, damage);
+        this.attackSpeed = attackSpeed;
+        this.cost = cost;
 
     }
 
