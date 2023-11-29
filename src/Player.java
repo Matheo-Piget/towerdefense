6db5e6beb3c96202f0c9d201c918a61c50a922d6
Mathéo Piget <matheo.piget5@gmail.com>
2023-11-29 11:07:30 +0100

    constructeur de player

--- src/Player.java
+++ src/Player.java
@@ -3,16 +3,18 @@ package src;
 public class Player {
 
     private int money;
+    private int lives;
 
-    public Player(int money) {
+    public Player(int money, int lives) {
 
         this.money = money;
+        this.lives = lives;
 
     }
 
     public void affiche() {
 
-        System.out.println("Vous avez : " + money);
+        System.out.println("Vous avez : " + money + " d'argent et " + lives + "vies");
 
     }
 
