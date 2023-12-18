8671c9966b7b5a3dc834917fd39a69928154354d
CHATER Mehdi <mehdi.chater@etu.u-paris.fr>
2023-12-18 15:51:35 +0100

    premier commit

--- src/main/java/configMap/Cellule.java
+++ src/main/java/configMap/Cellule.java
@@ -2,52 +2,46 @@ package src.main.java.configMap;
 
 import src.main.java.model.*;
 
+/**
+ * La classe Cellule représente une cellule de la carte du jeu.
+ * On pourra consulter la présence d'un élément sur la cellule, et le cas
+ * échéant, lequel.
+ */
+
 public class Cellule {
 
     private Element element;
 
     public Cellule() {
-
         element = null;
-
     }
 
     public Cellule(Element e) {
-
         element = e;
-
     }
 
+    /* Pour savoir si la cellule contient quelque chose */
     public boolean isOccupied() {
-
         return element != null;
-
     }
 
-    public Element get_elt() {
-
+    /* Pour récupérer dans la classe GameMap l'élement en question */
+    public Element getElement() {
         return element;
-
     }
 
-    public void set_elt(Element e) {
-
+    /* Pour modifier dans la classe GameMap l'élement pris en argument */
+    public void setElement(Element e) {
         this.element = e;
-
     }
 
+    /* Pour afficher l'élement sur le type de l'élement sur le tableau de jeu */
     public void affiche() {
-
         if (element instanceof Enemy) {
-
             System.out.print("E");
-
         } else if (element instanceof Tower) {
-
             System.out.print("T");
-
         } else {
-
             System.out.print("-");
 
         }
--- src/main/java/configMap/GameMap.java
+++ src/main/java/configMap/GameMap.java
@@ -24,7 +24,7 @@ public class GameMap {
         }
     }
 
-    public ArrayList<Enemy> tout_les_enemy(){ // retourne tous les enemies sous forme d'une liste, mieux manipulable
+    public ArrayList<Enemy> tout_les_enemy() { // retourne tous les enemies sous forme d'une liste, mieux manipulable
 
         ArrayList<Enemy> enemys = new ArrayList<Enemy>();
 
@@ -32,18 +32,17 @@ public class GameMap {
 
             for (int j = 0; j < tiles[0].length; j++) {
 
-                if(tiles[i][j].get_elt() instanceof Enemy) enemys.add((Enemy) tiles[i][j].get_elt()); // des que l'on a un enmis ou l'ajoute dasn la liste
+                if (tiles[i][j].getElement() instanceof Enemy)
+                    enemys.add((Enemy) tiles[i][j].getElement()); // des que l'on a un enmis ou l'ajoute dasn la liste
 
-                
             }
         }
 
         return enemys;
 
-
     }
 
-    public ArrayList<Tower> tout_les_tower(){ // retourne tous les tours sous forme d'une liste, mieux manipulable
+    public ArrayList<Tower> tout_les_tower() { // retourne tous les tours sous forme d'une liste, mieux manipulable
 
         ArrayList<Tower> towers = new ArrayList<Tower>();
 
@@ -51,31 +50,30 @@ public class GameMap {
 
             for (int j = 0; j < tiles[0].length; j++) {
 
-                if(tiles[i][j].get_elt() instanceof Tower) towers.add((Tower) tiles[i][j].get_elt()); // des que l'on a une tours ou l'ajoute dasn la liste
+                if (tiles[i][j].getElement() instanceof Tower)
+                    towers.add((Tower) tiles[i][j].getElement()); // des que l'on a une tours ou l'ajoute dasn la liste
 
-                
             }
         }
 
         return towers;
 
-
     }
 
-    public void tours_attaque(){ // fonction qui fait attaquer toutes les tours
+    public void tours_attaque() { // fonction qui fait attaquer toutes les tours
 
         for (Tower t : tout_les_tower()) {
-            
+
             t.attaque(this);
 
         }
 
     }
 
-    public void enemies_attaque(){ // fonction qui fait attaquer toutes les enemis
+    public void enemies_attaque() { // fonction qui fait attaquer toutes les enemis
 
         for (Enemy t : tout_les_enemy()) {
-            
+
             t.attaque(this);
 
         }
@@ -87,13 +85,14 @@ public class GameMap {
         int x = e.getX();
         int y = e.getY();
 
-        if (estDansLimites(x, y) && !tiles[y][x].isOccupied()) { // on regarde si les coordnonnées sont dans les limites et que la cellule en question est vide
+        if (estDansLimites(x, y) && !tiles[y][x].isOccupied()) { // on regarde si les coordnonnées sont dans les limites
+                                                                 // et que la cellule en question est vide
 
-            tiles[y][x].set_elt(e);
-            return true; 
+            tiles[y][x].setElement(e);
+            return true;
 
         }
-        
+
         return false;
 
     }
@@ -104,19 +103,19 @@ public class GameMap {
         int y = element.getY();
 
         if (estDansLimites(x, y)) {
-            tiles[y][x].set_elt(null);
+            tiles[y][x].setElement(null);
         } else {
             System.out.println("Coordonnées hors limites !");
         }
     }
 
-    public Tower trouverTowerSurMemeLigne(Enemy e){
+    public Tower trouverTowerSurMemeLigne(Enemy e) {
 
         int enemyY = e.getY();
 
         for (int i = tiles[enemyY].length - 1; i >= 0; i--) {
 
-            Element element = tiles[enemyY][i].get_elt();
+            Element element = tiles[enemyY][i].getElement();
 
             if (element instanceof Tower) {
 
@@ -126,7 +125,6 @@ public class GameMap {
         }
         return null; // Aucune tour sur la même ligne que la tour
 
-
     }
 
     public Enemy trouverEnnemiSurMemeLigne(Tower tower) {
@@ -135,7 +133,7 @@ public class GameMap {
 
         for (int y = 0; y < tiles[tourY].length; y++) {
 
-            Element element = tiles[tourY][y].get_elt();
+            Element element = tiles[tourY][y].getElement();
 
             if (element instanceof Enemy) {
 
@@ -156,10 +154,12 @@ public class GameMap {
             // Vérifie si la nouvelle position est libre
             if (!tiles[newY][newX].isOccupied()) {
 
-                tiles[oldY][oldX].set_elt(null);; // Supprime l'élément de son ancienne position
+                tiles[oldY][oldX].setElement(null);
+                ; // Supprime l'élément de son ancienne position
                 element.setX(newX);
                 element.setY(newY);
-                tiles[newY][newX].set_elt(element);; // Place l'élément à sa nouvelle position
+                tiles[newY][newX].setElement(element);
+                ; // Place l'élément à sa nouvelle position
                 return true;
 
             } else {
@@ -175,36 +175,37 @@ public class GameMap {
         }
     }
 
-    public void enemyMort(){ // supprime tout les enemies qui n'ont plus de vie
+    public void enemyMort() { // supprime tout les enemies qui n'ont plus de vie
 
         for (Enemy e : tout_les_enemy()) {
-            
-            if(e.getHealth() <= 0) retirerElement(e);
+
+            if (e.getHealth() <= 0)
+                retirerElement(e);
 
         }
 
     }
 
-    public void towerMorte(){ // supprime tout les tours qui n'ont plus de vie
+    public void towerMorte() { // supprime tout les tours qui n'ont plus de vie
 
         for (Tower t : tout_les_tower()) {
-            
-            if(t.getHealth() <= 0) retirerElement(t);
+
+            if (t.getHealth() <= 0)
+                retirerElement(t);
 
         }
 
     }
 
-    public void update(){
+    public void update() {
 
         tours_attaque();
         enemies_attaque();
         enemyMort(); // on supprime tout les enemis mort
         towerMorte(); // meme chose pour les tours
         deplacerTousLesEnnemis(); // on déplace tout les enemis
-        nouveauxEnemy(); //TODO -> faire apparaître de nouveaux enemies suivant un timer ou autre alternative
-
-        
+        nouveauxEnemy(); // TODO -> faire apparaître de nouveaux enemies suivant un timer ou autre
+                         // alternative
 
     }
 
@@ -212,24 +213,24 @@ public class GameMap {
     public void deplacerTousLesEnnemis() {
 
         for (Enemy e : tout_les_enemy()) {
-            
+
             deplacerEnnemi(e);
 
         }
 
     }
 
-    public void nouveauxEnemy(){
+    public void nouveauxEnemy() {
 
         int mapWidth = tiles[0].length;
         int mapHeight = tiles.length;
         int numberOfEnemies = 2; // Nombre d'ennemis à placer (à ajuster selon vos besoins)
-    
+
         for (int i = 0; i < numberOfEnemies; i++) {
             int randomX = mapWidth - 1; // Position aléatoire sur la dernière colonne de la carte
             int randomY = (int) (Math.random() * mapHeight); // Position aléatoire sur la hauteur de la carte
-    
-            placer(new Enemy(1, 2, 1, randomX, randomY)); 
+
+            placer(new Enemy(1, 2, 1, randomX, randomY));
         }
 
     }
@@ -243,7 +244,8 @@ public class GameMap {
         // Calcul des nouvelles positions
         int newX = enemy.getX() - deplacementX;
 
-        // Vérification si les nouvelles positions sont à l'intérieur des limites de la carte
+        // Vérification si les nouvelles positions sont à l'intérieur des limites de la
+        // carte
         if (estDansLimites(newX, enemy.getY())) {
 
             deplacerElement(enemy, newX, enemy.getY());// Déplacement de l'ennemi aux nouvelles positions
@@ -274,7 +276,8 @@ public class GameMap {
 
     private boolean estDansLimites(int x, int y) {
 
-        return x >= 0 && x < tiles[0].length && y >= 0 && y < tiles.length; // teste si les coordenées sont dans les limites
+        return x >= 0 && x < tiles[0].length && y >= 0 && y < tiles.length; // teste si les coordenées sont dans les
+                                                                            // limites
 
     }
 
