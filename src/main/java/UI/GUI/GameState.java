4e3714a2837977d83a7f15d8b4fbe66dd03eca54
Mathéo Piget <matheo.piget5@gmail.com>
2024-01-05 14:39:27 +0100

    test

--- src/main/java/UI/GUI/GameState.java
+++ src/main/java/UI/GUI/GameState.java
@@ -7,7 +7,6 @@ import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
 import java.io.File;
 import java.io.IOException;
 
@@ -22,10 +21,8 @@ import javax.swing.JPanel;
 import javax.swing.Timer;
 import javax.swing.border.Border;
 
-import org.w3c.dom.events.MouseEvent;
 
 import src.main.java.configMap.GameMap;
-import src.main.java.model.Tower;
 import src.main.java.start.Player;
 
 /**
